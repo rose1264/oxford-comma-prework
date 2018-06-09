217aafd81c3a0cb2d5189bb91fc5dda10d70e30d
Shumei Huang <rose1264@gmail.com>
2018-06-09 13:22:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,8 @@ def oxford_comma(array)
     array[0]
   elsif array.length == 2
     array.join(" and ")
+  elsif array.length == 3
+    array[-1].insert(0, "and ")
+    array.join(", ")
   end
 end
