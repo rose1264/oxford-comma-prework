5f7dbcc33036b17d7a77cd791d36a74105cff0b7
Shumei Huang <rose1264@gmail.com>
2018-06-09 13:23:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array[0]
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
     array[-1].insert(0, "and ")
     array.join(", ")
   end
